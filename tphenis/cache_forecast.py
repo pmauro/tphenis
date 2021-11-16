054535a57f1231fc8635ee46ecb06927bb291180
Patrick Mauro <patrick@gordiandata.com>
2021-11-16 09:56:47 -0800

    Add command line arguments.

--- tphenis/cache_forecast.py
+++ tphenis/cache_forecast.py
@@ -12,20 +12,47 @@ import util.wxenums as wxenums
 import util.fcast_cache as fcast_cache
 
 LOGGER = logging.getLogger('tphenis')
-logging.basicConfig(format='%(asctime)s %(levelname)s - %(module)s.%(funcName)s() - %(message)s ',
+logging.basicConfig(format='%(asctime)s %(levelname)-8s - %(module)s.%(funcName)s() - %(message)s ',
                     datefmt= '%Y%m%d %H:%M:%S')
 
 # ---------------------------------------------------------------------------------------------------------------------
 # main()
 # ---------------------------------------------------------------------------------------------------------------------
 
-source = wxenums.ForecastSource.MORA_REC_FCST
-location = wxenums.Location.MORA
-log_level = logging.INFO
-# todo Get source and location from command line, log level too
 
-LOGGER.setLevel(log_level)
+def main():
+    arg_parser = argparse.ArgumentParser(description="Request a raw forecast and cache it if it's new.")
 
-# todo Create an option to force an overwrite?
-fcast_cache.get_raw_forecast(source, location, use_cache=False, save_forecast=True)
+    arg_parser.add_argument('--src', action='store', required=True, help='forecast source')
+    arg_parser.add_argument('--loc', action='store', required=True, help='forecast location')
+    arg_parser.add_argument('--log-level', action='store', required=False, default='INFO',
+                            choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                            help="Set the logging level",
+                            dest='loglevel')
 
+    args = arg_parser.parse_args()
+
+    numeric_level = getattr(logging, args.loglevel.upper())
+    if numeric_level is None:
+        LOGGER.critical("Invalid log level specified: {}".format(args.loglevel))
+        exit(1)
+    LOGGER.setLevel(numeric_level)
+
+    # Validate user input for the ForecastSource and Location
+    try:
+        source = wxenums.ForecastSource[args.src]
+    except KeyError:
+        LOGGER.critical("Invalid forecast source specified: {}".format(args.src))
+        exit(1)
+
+    try:
+        location = wxenums.Location[args.loc]
+    except KeyError:
+        LOGGER.critical("Invalid forecast location specified: {}".format(args.loc))
+        exit(1)
+
+    # todo Create an option to force an overwrite?
+    fcast_cache.get_raw_forecast(source, location, use_cache=False, save_forecast=True)
+
+
+main()
